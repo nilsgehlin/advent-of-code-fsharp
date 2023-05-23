853fd13d3fa97df94e742db0dc15dde06c11f08b
Nils Gehlin <nils.gehlin@stadium.com>
2023-05-23 20:31:52 +0200

    day1 complete

---
+++ csharp/Day1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+var data = File.ReadAllText("input.txt");
+var query =
+    from calorieList in data.Split($"{Environment.NewLine}{Environment.NewLine}")
+    let caloriePerElf = calorieList.Split(Environment.NewLine).Select(int.Parse).Sum()
+    orderby caloriePerElf descending
+    select caloriePerElf;
+
+var answerPartOne = query.First();
+Console.WriteLine($"Answer Part One: {answerPartOne}");
+
+var answerPartTwo = query.Take(3).Sum();
+Console.WriteLine($"Answer Part Two: {answerPartTwo}");
