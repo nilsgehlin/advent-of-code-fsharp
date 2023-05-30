26715cc3f0f9a26ed1be022e8e6b75b812907aa6
Nils Gehlin <nils.gehlin@gmail.com>
2023-05-30 18:30:07 +0200

    day3 skeleton

---
+++ csharp/Day3/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.IO;
+using System.Text;
+
+namespace Day3;
+class Program
+{
+    static void Main()
+    {
+        var data = File.ReadAllText("input.txt");
+    }
+}
+
+
+
