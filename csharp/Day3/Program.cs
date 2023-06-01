b3a94c416aeb371b8eb530c933280b4a4623cf95
Nils Gehlin <nils.gehlin@gmail.com>
2023-06-01 08:09:02 +0200

    started day3

--- csharp/Day3/Program.cs
+++ csharp/Day3/Program.cs
@@ -9,6 +9,23 @@ class Program
     static void Main()
     {
         var data = File.ReadAllText("input.txt");
+        var answerPartOne = data.Split($"{Environment.NewLine}").Select(CalculatePrio).Sum();
+        Console.WriteLine($"Answer Part One: {answerPartOne}");
+    }
+
+    static int CalculatePrio(string line)
+    {
+        var items = line.ToCharArray();
+        var compartmentSize = items.Length;
+
+
+
+        // foreach (var item in items)
+        // {
+        //     System.Console.Write($"{item}, ");
+        // }
+        // System.Console.WriteLine();
+        return 0;
     }
 }
 
