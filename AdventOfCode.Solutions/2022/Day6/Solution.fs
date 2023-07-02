7d466483dd17721836123717b2b8ec87923a36bf
Nils Gehlin <nils.gehlin@gmail.com>
2023-07-02 21:44:25 +0200

    day6 completed

---
+++ AdventOfCode.Solutions/2022/Day6/Solution.fs
@@ -0,0 +1,23 @@
+﻿namespace AdventOfCode.Solutions.Day6
+
+open AdventOfCode.Common
+
+module Solution =
+    let findNDistinctChars n input =
+        let areAllCharsUnique chars =
+            let distinctElements = List.distinct chars
+            List.length chars = List.length distinctElements
+
+        input
+        |> Seq.toList
+        |> List.windowed n
+        |> List.findIndex areAllCharsUnique
+        |> (+) n
+
+    [<Solution(2022, 6, 1)>]
+    let solve1 (input: string) =
+        findNDistinctChars 4 input
+
+    [<Solution(2022, 6, 2)>]
+    let solve2 (input: string) =
+        findNDistinctChars 14 input
